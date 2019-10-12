d999ec05f8377a6347c9bd61cef9d4b97eabbb6e
athanasiouconst <csiouath@gmail.com>
2019-10-12 20:01:19 +0300

    create components

---
+++ src/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Content.css';
+
+function Content () {
+        return (
+            <div className="content">
+                <p>Content</p>
+            </div>
+        );
+}
+
+export default Content;
