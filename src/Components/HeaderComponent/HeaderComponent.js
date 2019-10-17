2fd1e2b90a9d3cddf3eda12374d6ee283e9f48bb
athanasiouconst <csiouath@gmail.com>
2019-10-17 20:05:35 +0300

    enable menu dynamically

--- src/Components/HeaderComponent/HeaderComponent.js
+++ src/Components/HeaderComponent/HeaderComponent.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Link} from 'react-router-dom';
 import AuthenticationService from "../SuperFoodsApp/AuthenticationService";
-
+import { withRouter } from 'react-router';
 
 class HeaderComponent extends Component {
     render() {
